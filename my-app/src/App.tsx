d6000577bd8cb635a41481464a5942b0a148dcec
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-06-22 19:03:14 -0400

    argocd 6

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -335,7 +335,7 @@ const App: React.FC = () => {
         <div className="login-card">
           <h1>Cloud Manager By Manab</h1>
           <p className="subtext">
-            Argocd 5
+            Argocd 6
           </p>
           <a
             href="https://cloud.google.com/"
