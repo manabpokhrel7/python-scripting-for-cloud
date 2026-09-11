50a65e986693cfe25c923dba48add772b4e17692
manabpokhrel7 <65856123+manabpokhrel7@users.noreply.github.com>
2026-09-11 17:39:18 -0400

    added ollama

--- GCP/AI/aitest.py
+++ GCP/AI/aitest.py
@@ -1,3 +1,5 @@
+import asyncio
+from ollama import AsyncClient
 import json
 from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
@@ -7,38 +9,41 @@ from cache.redis import r
 from starlette.requests import Request
 from logger import logger
 
-
 router = APIRouter(tags=["ai"])
-client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
 class AIRequest(BaseModel):
     input_text: str
+
+
+client = AsyncClient(host='https://ollama.manabpokhrel.com.np')
+
 
 @router.post("/response")
 async def response(payload: AIRequest, request: Request):
     try:
         previous_convo = r.get(request.session.get('sub'))
         print(f"\n\nthis is the previous convo {previous_convo}")
-        input_list = [{"role": "developer", "content": "This is a cloud platform designed to create VMS in GCP only using the API Manab Designed"}]
+        input_list = [{"role": "system",
+                       "content": "This is a cloud platform designed to create VMS in GCP only using the API Manab Designed"}]
         if previous_convo:
             input_list.extend(json.loads(previous_convo))
-        user_input = [{"role" : "user", "content": payload.input_text}]
+        user_input = [
+            {
+                'role': 'user',
+                'content': payload.input_text,
+            },
+        ]
         input_list.extend(user_input)
-        result = await client.responses.create(
-            model="gpt-5-nano",
-            input=input_list
-        )
-        output_text = [{"role" : "assistant", "content": result.output_text}]
+        result = await client.chat('qwen3:4b', messages=input_list)
+        output_text = [{"role": "assistant", "content": result.message.content}]
         input_list.extend(output_text)
-        #Trimming the input list before saving
+        # Trimming the input list before saving
         MAX_MESSAGES = 20
         if len(input_list) > MAX_MESSAGES:
             input_list = input_list[-MAX_MESSAGES:]
-        #End of trim
+        # End of trim
         r.set(request.session.get('sub'), json.dumps(input_list))
-        return {"response": result.output_text}
+        return {"response": result.message.content}
     except Exception as e:
         logger.exception(e)
         raise HTTPException(status_code=500, detail=str(e))
-
-
---
+++ GCP/AI/aitestbak.py
@@ -0,0 +1,44 @@
+# import json
+# from fastapi import APIRouter, HTTPException
+# from pydantic import BaseModel
+# from openai import AsyncOpenAI
+# import os
+# from cache.redis import r
+# from starlette.requests import Request
+# from logger import logger
+#
+#
+# router = APIRouter(tags=["ai"])
+# client = AsyncOpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+#
+# class AIRequest(BaseModel):
+#     input_text: str
+#
+# @router.post("/response")
+# async def response(payload: AIRequest, request: Request):
+#     try:
+#         previous_convo = r.get(request.session.get('sub'))
+#         print(f"\n\nthis is the previous convo {previous_convo}")
+#         input_list = [{"role": "developer", "content": "This is a cloud platform designed to create VMS in GCP only using the API Manab Designed"}]
+#         if previous_convo:
+#             input_list.extend(json.loads(previous_convo))
+#         user_input = [{"role" : "user", "content": payload.input_text}]
+#         input_list.extend(user_input)
+#         result = await client.responses.create(
+#             model="gpt-5-nano",
+#             input=input_list
+#         )
+#         output_text = [{"role" : "assistant", "content": result.output_text}]
+#         input_list.extend(output_text)
+#         #Trimming the input list before saving
+#         MAX_MESSAGES = 20
+#         if len(input_list) > MAX_MESSAGES:
+#             input_list = input_list[-MAX_MESSAGES:]
+#         #End of trim
+#         r.set(request.session.get('sub'), json.dumps(input_list))
+#         return {"response": result.output_text}
+#     except Exception as e:
+#         logger.exception(e)
+#         raise HTTPException(status_code=500, detail=str(e))
+#
+#
